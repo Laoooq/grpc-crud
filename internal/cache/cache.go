204abb36e306cd59ec344fdebea196006358861b
tienlao <tienlao004@gmail.com>
2024-06-22 19:21:49 +0300

    Cache realisation

--- internal/cache/cache.go
+++ internal/cache/cache.go
@@ -1,1 +1,58 @@
 package cache
+
+import (
+	"database/sql"
+	"grpc-crud/cmd/api"
+	"sync"
+	"time"
+)
+
+type Server struct {
+	api.UnimplementedUserServiceServer
+	users map[string]*api.User
+	cache Cache
+	db    *sql.DB
+	mutex sync.Mutex
+}
+
+type Cache struct {
+	items map[string]Item
+	mu    sync.Mutex
+}
+
+type Item struct {
+	Value      interface{}
+	Expiration int64
+}
+
+func NewCache() *Cache {
+	return &Cache{items: make(map[string]Item)}
+}
+
+func (c *Cache) Set(key string, value interface{}, duration time.Duration) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	expiration := time.Now().Add(duration).UnixNano()
+	c.items[key] = Item{Value: value, Expiration: expiration}
+}
+
+func (c *Cache) Get(key string) (interface{}, bool) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	item, found := c.items[key]
+	if !found {
+		return nil, false
+	}
+	if time.Now().UnixNano() > item.Expiration {
+		delete(c.items, key)
+		return nil, false
+	}
+	return item.Value, true
+}
+
+func (c *Cache) Delete(key string) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	delete(c.items, key)
+}
