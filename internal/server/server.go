64965de5aa6129025fc543c7887b7d5d15003d21
tienlao <tienlao004@gmail.com>
2024-06-25 14:39:51 +0300

    add a mock generation

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,3 +1,21 @@
 package server
 
-//go:generate mockgen -destination=mocks/DBMock.go -package=mocks github.com/tienlaoq/grpc-crud/internal/server DB
+import (
+	"context"
+	"database/sql"
+	"time"
+)
+
+//go:generate mockgen -destination=mocks/DBMock.go -package=mocks grpc-crud/internal/server DB
+type DB interface {
+	QueryRow(query string, args ...interface{}) *sql.Row
+	Exec(query string, args ...interface{}) (sql.Result, error)
+	QueryLowContext(ctx context.Context, query string, args ...interface{}) (*sql.Rows, error)
+}
+
+//go:generate mockgen -destination=mocks/CacheMock.go -package=mocks grpc-crud/internal/server Cache
+type Cache interface {
+	Set(key string, value interface{}, duration time.Duration)
+	Get(key string) (interface{}, bool)
+	Delete(key string)
+}
